b6d7e202ed9d70a8c938b6865c900703b2201159
fayeoyaee <ouyang.f@husky.neu.edu>
2018-02-20 12:26:36 -0800

    add angular routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-
+// My components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/user/login/login.component';
 import { ProfileComponent } from './components/user/profile/profile.component';
@@ -19,6 +21,8 @@ import { WidgetHeaderComponent } from './components/widget/widget-edit/widget-he
 import { WidgetImageComponent } from './components/widget/widget-edit/widget-image/widget-image.component';
 import { WidgetYoutubeComponent } from './components/widget/widget-edit/widget-youtube/widget-youtube.component';
 
+// import routing
+import { Routing } from './app.routing';
 
 @NgModule({
   declarations: [
---
+++ src/app/app.routing.ts
@@ -0,0 +1,39 @@
+import {Routes, RouterModule} from "@angular/router";
+import {ModuleWithProviders} from "@angular/core";
+import {LoginComponent} from "./components/user/login/login.component";
+import {RegisterComponent} from "./components/user/register/register.component";
+import {ProfileComponent} from "./components/user/profile/profile.component";
+import {PageEditComponent} from "./components/page/page-edit/page-edit.component";
+import {PageListComponent} from "./components/page/page-list/page-list.component";
+import {PageNewComponent} from "./components/page/page-new/page-new.component";
+import {WebsiteEditComponent} from "./components/website/website-edit/website-edit.component";
+import {WebsiteListComponent} from "./components/website/website-list/website-list.component";
+import {WebsiteNewComponent} from "./components/website/website-new/website-new.component";
+import {WidgetChooserComponent} from "./components/widget/widget-chooser/widget-chooser.component";
+import {WidgetListComponent} from "./components/widget/widget-list/widget-list.component";
+import {WidgetHeaderComponent} from "./components/widget/widget-edit/widget-header/widget-header.component";
+import {WidgetImageComponent} from "./components/widget/widget-edit/widget-image/widget-image.component";
+import {WidgetYoutubeComponent} from "./components/widget/widget-edit/widget-youtube/widget-youtube.component";
+import {WidgetEditComponent} from "./components/widget/widget-edit/widget-edit.component";
+// Import all other components here
+const APP_ROUTES : Routes = [
+// { path : '', component : HomeComponent},
+{ path : 'login' , component: LoginComponent},
+{ path : 'register' , component: RegisterComponent },
+{ path : 'user/:userId' , component: ProfileComponent},
+{ path : 'user/:userId/website' , component: WebsiteListComponent},
+{ path : 'user/:userId/website/new' , component: WebsiteNewComponent},
+{ path : 'user/:userId/website/:wid' , component: WebsiteEditComponent},
+{ path : 'user/:userId/website/:wid/page' , component: PageListComponent},
+{ path : 'user/:userId/website/:wid/page/new' , component: PageNewComponent},
+{ path : 'user/:userId/website/:wid/page/:pageId' , component: PageEditComponent},
+{ path : 'user/:userId/website/:wid/page/:pageId/widget' , component: WidgetListComponent},
+{ path : 'user/:userId/website/:wid/page/:pageId/widget/new' , component: WidgetChooserComponent},
+{ path : 'user/:userId/website/:wid/page/:pageId/widget/:widgetId' , component: WidgetEditComponent},
+{ path : 'user/:userId/website/:wid/page/:pageId/widget/:widgetId/header' , component: WidgetHeaderComponent},
+{ path : 'user/:userId/website/:wid/page/:pageId/widget/:widgetId/image' , component: WidgetImageComponent},
+{ path : 'user/:userId/website/:wid/page/:pageId/widget/:widgetId/youtube' , component: WidgetYoutubeComponent},
+];
+
+// Export the routes as module providers
+export const Routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
