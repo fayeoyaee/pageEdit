916fa5c5cb04b0a93ec1ff1fadced1d177fef786
fayeoyaee <ouyang.f@husky.neu.edu>
2018-03-04 11:50:12 -0800

    rebuild login form/component

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  // access the form using @ViewChild which allows to access a local reference
+  // the instance of @ViewChild will be of type NgForm
+  @ViewChild('f') loginForm: NgForm;
+
+  // properties 
+  username: String;
+  password: String;
+  errorFlag: boolean;
+  errorMsg: 'Invalid username or password!';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login() {
+    // fetching data from loginForm using @ViewChild reference created in above steps
+    this.username = this.loginForm.value.username;
+    this.password = this.loginForm.value.password;
   }
 
 }
