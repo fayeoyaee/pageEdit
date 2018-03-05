cc7fa7920ae6dcbff3d9abd24913bf4326605631
fayeoyaee <ouyang.f@husky.neu.edu>
2018-03-04 16:32:16 -0800

    populate page edit page

--- src/app/components/page/page-edit/page-edit.component.ts
+++ src/app/components/page/page-edit/page-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PageService } from '../../../services/page.service.client';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-page-edit',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-edit.component.css']
 })
 export class PageEditComponent implements OnInit {
+  // properties 
+  pageId: String;
+  page = {};
+  pagename: String;
+  description: String;
 
-  constructor() { }
+  constructor(private pageService: PageService, private activateRoute: ActivatedRoute) { }
 
   ngOnInit() {
+
+    // retrieves userId as path parameter
+    this.activateRoute.params.subscribe(
+      (params: any) => {
+        this.pageId = params['pageId'];
+      }
+    );
+
+    // user UserService to retrieve the user instance
+    this.page = this.pageService.findPageById(this.pageId);
+    this.pagename = this.page['name'];
+    this.description = this.page['description'];
   }
 
 }
