4b8ff73c614273fb533258aa44f70a17ee9a9019
fayeoyaee <ouyang.f@husky.neu.edu>
2018-03-04 16:27:37 -0800

    populate website edit page

--- src/app/components/website/website-edit/website-edit.component.ts
+++ src/app/components/website/website-edit/website-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WebsiteService } from '../../../services/website.service.client';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-website-edit',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./website-edit.component.css']
 })
 export class WebsiteEditComponent implements OnInit {
+  // properties 
+  websiteId: String;
+  website = {};
+  websitename: String;
+  description: String;
 
-  constructor() { }
+  constructor(private websiteService: WebsiteService, private activateRoute: ActivatedRoute) { }
 
   ngOnInit() {
+
+    // retrieves userId as path parameter
+    this.activateRoute.params.subscribe(
+      (params: any) => {
+        this.websiteId = params['websiteId'];
+      }
+    );
+
+    // user UserService to retrieve the user instance
+    this.website = this.websiteService.findWebsiteById(this.websiteId);
+    this.websitename = this.website['name'];
+    this.description = this.website['description'];
   }
 
 }
