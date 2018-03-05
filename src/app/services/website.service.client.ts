e13511a576b4685130f0ad41f24cbcc5ff9d7705
fayeoyaee <ouyang.f@husky.neu.edu>
2018-03-04 19:37:25 -0800

    populate widget image edit page

--- src/app/services/website.service.client.ts
+++ src/app/services/website.service.client.ts
@@ -9,9 +9,9 @@ export class WebsiteService {
     constructor() { }
 
     websites = [
-        { _id: "123", name: "Facebook", developerId: "456", description: "Lorem"},
-        { _id: "234", name: "Tweeter", developerId: "456", description: "Lorem"},
-        { _id: "456", name: "Gizmodo", developerId: "456", description: "Lorem"},
+        { _id: "123", name: "Facebook", developerId: "123", description: "Lorem"},
+        { _id: "234", name: "Tweeter", developerId: "123", description: "Lorem"},
+        { _id: "456", name: "Gizmodo", developerId: "123", description: "Lorem"},
         { _id: "890", name: "Go", developerId: "123", description: "Lorem"},
         { _id: "567", name: "Tic Tac Toe", developerId: "123", description: "Lorem"},
         { _id: "678", name: "Checkers", developerId: "123", description: "Lorem"},
